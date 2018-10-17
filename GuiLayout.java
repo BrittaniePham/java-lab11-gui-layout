5963923d2c57673d59521bae41d58e678703331e
Brittanie Pham <brittaniepham@gmail.com>
2018-10-17 11:36:17 -0600

    basic layout

--- GuiLayout.java
+++ GuiLayout.java
@@ -6,6 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class GuiLayout extends JFrame {
 
@@ -35,8 +43,60 @@ public class GuiLayout extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Demo Layout");
+		lblNewLabel.setBorder(new EmptyBorder(10, 0, 10, 0));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.WEST);
+		panel.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JButton btnNewButton = new JButton("==>");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("<==");
+		panel.add(btnNewButton_1);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JLabel label = new JLabel("1");
+		label.setOpaque(true);
+		label.setBackground(Color.YELLOW);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
+		panel_1.add(label);
+		
+		JLabel label_1 = new JLabel("2");
+		label_1.setBackground(Color.BLUE);
+		label_1.setOpaque(true);
+		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+		label_1.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
+		panel_1.add(label_1);
+		
+		JLabel label_2 = new JLabel("3");
+		label_2.setOpaque(true);
+		label_2.setBackground(Color.YELLOW);
+		label_2.setHorizontalAlignment(SwingConstants.CENTER);
+		label_2.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
+		panel_1.add(label_2);
+		
+		JLabel label_3 = new JLabel("4");
+		label_3.setBackground(Color.YELLOW);
+		label_3.setOpaque(true);
+		label_3.setHorizontalAlignment(SwingConstants.CENTER);
+		label_3.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
+		panel_1.add(label_3);
 	}
 
 }
