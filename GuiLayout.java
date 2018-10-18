2981ae1fdac3cd07801815a6803273812bd28dde
Brittanie Pham <brittaniepham@gmail.com>
2018-10-18 12:31:06 -0600

    loop change

--- GuiLayout.java
+++ GuiLayout.java
@@ -12,12 +12,14 @@ import javax.swing.SwingConstants;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 
 public class GuiLayout extends JFrame {
 
 	private JPanel contentPane;
+	private ArrayList<JLabel> labels;
 
 	/**
 	 * Launch the application.
@@ -56,47 +58,66 @@ public class GuiLayout extends JFrame {
 		contentPane.add(panel, BorderLayout.WEST);
 		panel.setLayout(new GridLayout(2, 0, 0, 0));
 		
-		JButton btnNewButton = new JButton("==>");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("<==");
-		panel.add(btnNewButton_1);
-		
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(new GridLayout(1, 0, 0, 0));
 		
-		JLabel label = new JLabel("1");
-		label.setOpaque(true);
-		label.setBackground(Color.YELLOW);
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
-		panel_1.add(label);
-		
-		JLabel label_1 = new JLabel("2");
-		label_1.setBackground(Color.BLUE);
+		JLabel label_1 = new JLabel("1");
 		label_1.setOpaque(true);
+		label_1.setBackground(Color.YELLOW);
 		label_1.setHorizontalAlignment(SwingConstants.CENTER);
 		label_1.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
 		panel_1.add(label_1);
 		
-		JLabel label_2 = new JLabel("3");
+		JLabel label_2 = new JLabel("2");
+		label_2.setBackground(Color.BLUE);
 		label_2.setOpaque(true);
-		label_2.setBackground(Color.YELLOW);
 		label_2.setHorizontalAlignment(SwingConstants.CENTER);
 		label_2.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
 		panel_1.add(label_2);
 		
-		JLabel label_3 = new JLabel("4");
-		label_3.setBackground(Color.YELLOW);
+		JLabel label_3 = new JLabel("3");
 		label_3.setOpaque(true);
+		label_3.setBackground(Color.YELLOW);
 		label_3.setHorizontalAlignment(SwingConstants.CENTER);
 		label_3.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
 		panel_1.add(label_3);
+		
+		JLabel label_4 = new JLabel("4");
+		label_4.setBackground(Color.YELLOW);
+		label_4.setOpaque(true);
+		label_4.setHorizontalAlignment(SwingConstants.CENTER);
+		label_4.setFont(new Font("Lucida Grande", Font.PLAIN, 31));
+		panel_1.add(label_4);
+		
+		labels = new ArrayList<>();
+		labels.add(label_1);
+		labels.add(label_2);
+		labels.add(label_3);
+		labels.add(label_4);
+		
+		JButton btnNewButton = new JButton("==>");
+		JButton btnNewButton_1 = new JButton("<==");
+		panel.add(btnNewButton_1);
+		panel.add(btnNewButton);
+		
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int currentBlue = 0;
+				for(int i = 0; i < labels.size(); ++i) {					
+					if(labels.get(i).getBackground() == Color.BLUE) {
+						currentBlue = i;
+					}	
+				}
+				if(currentBlue == 0) {
+					labels.get(labels.size() - 1).setBackground(Color.BLUE);
+					labels.get(0).setBackground(Color.YELLOW);
+				} else {
+					labels.get(currentBlue - 1).setBackground(Color.BLUE);
+					labels.get(currentBlue).setBackground(Color.YELLOW);
+				}
+			}
+		});
 	}
 
 }
